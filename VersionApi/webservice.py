973f763ae2133ee3a4310e557d00f3e54bf94d97
Kenan BAŞDEMİR <kenan.basdemir5858@gmail.com>
2020-05-10 13:35:11 +0300

    python add

---
+++ VersionApi/webservice.py
@@ -0,0 +1,17 @@
+# import flask dependencies
+from flask import Flask
+# initialize the flask app
+app = Flask(__name__)
+# default route
+@app.route('/')
+def index():
+    return 'Hello World!'
+# create a route for webhook
+@app.route('/api/version')
+def hello():
+    return 'Version: 1.9.2'
+
+# run the app
+if __name__ == '__main__':
+   #app.run()
+   app.run(host='0.0.0.0', port=7003)
